787c31162c91b7ab95e3bdc89a9305ae92e90313
David Ben-Yaakov <david@tangogolfdigital.com>
2019-07-24 13:21:53 -0500

    Updated library path

--- make.sh
+++ make.sh
@@ -3,5 +3,8 @@ if [[ -z "$LD_LIBRARY_PATH" ]]; then
     echo "Must provide LD_LIBRARY_PATH in environment" 1>&2
     exit 1
 fi
-echo "LD_LIBRARY_PATH at $LD_LIBRARY_PATH"
-swift build -Xswiftc -I -Xswiftc $LD_LIBRARY_PATH -Xswiftc -L -Xswiftc $LD_LIBRARY_PATH -Xswiftc -lIgis
+if [[ -z "$IGIS_LIBRARY_PATH" ]]; then
+    echo "Must provide IGIS_LIBRARY_PATH in environment" 1>&2
+    exit 1
+fi
+swift build -Xswiftc -I -Xswiftc $IGIS_LIBRARY_PATH -Xswiftc -L -Xswiftc $IGIS_LIBRARY_PATH -Xswiftc -lIgis

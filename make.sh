9237b6523f996618ec783a0f9f2435c901b4cea4
David Ben-Yaakov <david@tangogolfdigital.com>
2019-08-29 11:00:45 -0500

    Corrected paths

--- make.sh
+++ make.sh
@@ -7,4 +7,6 @@ if [[ -z "$IGIS_LIBRARY_PATH" ]]; then
     echo "Must provide IGIS_LIBRARY_PATH in environment" 1>&2
     exit 1
 fi
+echo "IGIS_LIBRARY_PATH at $IGIS_LIBRARY_PATH"
+echo "LD_LIBRARY_PATH at $LD_LIBRARY_PATH"
 swift build -Xswiftc -I -Xswiftc $IGIS_LIBRARY_PATH -Xswiftc -L -Xswiftc $IGIS_LIBRARY_PATH -Xswiftc -lIgis
--- run.sh
+++ run.sh
@@ -3,5 +3,10 @@ if [[ -z "$LD_LIBRARY_PATH" ]]; then
     echo "Must provide LD_LIBRARY_PATH in environment" 1>&2
     exit 1
 fi
+if [[ -z "$IGIS_LIBRARY_PATH" ]]; then
+    echo "Must provide IGIS_LIBRARY_PATH in environment" 1>&2
+    exit 1
+fi
+echo "IGIS_LIBRARY_PATH at $IGIS_LIBRARY_PATH"
 echo "LD_LIBRARY_PATH at $LD_LIBRARY_PATH"
-swift run -Xswiftc -I -Xswiftc $LD_LIBRARY_PATH -Xswiftc -L -Xswiftc $LD_LIBRARY_PATH -Xswiftc -lIgis
+swift run -Xswiftc -I -Xswiftc $IGIS_LIBRARY_PATH -Xswiftc -L -Xswiftc $IGIS_LIBRARY_PATH -Xswiftc -lIgis

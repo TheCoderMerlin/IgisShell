3881d68a711fe907b8e59056e73b063cd3b5d0f6
David Ben-Yaakov <david@tangogolfdigital.com>
2022-10-28 09:45:24 -0500

    Updated for latest Igis version
    * Updated to Igis 1.4.0
    * Updated to swift-tools-version:5.7
    * Removed .dir-locals.el from repository
    * Added newly required Swift Atomics

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,11 +6,13 @@ import PackageDescription
 let package = Package(
     name: "IgisShell",
     dependencies: [
+      .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "IgisShell"),
+        .executableTarget(
+          name: "IgisShell",
+          dependencies: [.product(name: "Atomics", package: "swift-atomics")])
     ]
 )

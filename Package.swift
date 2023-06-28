ae58f7524495237565e44746c0bdd0b2d5bf2255
David Ben-Yaakov <david@codermerlin.academy>
2023-06-28 10:50:18 -0500

    Updated dependency Swift-Atomics 1.1.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "IgisShell",
     dependencies: [
-      .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.2")
+      .package(url: "https://github.com/apple/swift-atomics.git", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
